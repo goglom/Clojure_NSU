56d42021358c8d8b63cdbaab40278082a3fa3285
goglom <goglom12@gmail.com>
2023-02-26 17:56:24 +0700

    * passed task 1.1 * add new rule to gitignore

--- problem_1.1.clj
+++ problem_1.1.clj
@@ -2,9 +2,9 @@
 (defn append-char
   ([alpahbet input-str result-l]
    (if (> (count alpahbet) 0)
-     (if (= (first alpahbet) (subs input-str (- (.length input-str) 1)))
+     (if (= (first alpahbet) (last input-str))
        (append-char (rest alpahbet) input-str result-l)
-       (append-char (rest alpahbet) input-str (cons (.concat input-str (first alpahbet)) result-l)))
+       (append-char (rest alpahbet) input-str (cons (str input-str (first alpahbet)) result-l)))
      result-l))
   ([alpahbet input-str]
    (append-char alpahbet input-str ())))
@@ -22,16 +22,16 @@
 
 (defn main
   ([alphabet num result-l]
-   (if (> num 1)
+   (if (> num 0)
      (main alphabet (- num 1) (multistring-append-char
                                alphabet result-l))
      result-l))
-  ([alphabet num] (main alphabet num alphabet)))
+  ([alphabet num] (main alphabet num (list ""))))
 
-(def alpha `("a", "b", "c"))
+(def alpha `(\a, \b, \c))
 
 (append-char alpha "ab")
 
 (multistring-append-char alpha `("ab"))
 
-(main alpha 2)
+(main alpha 3)
